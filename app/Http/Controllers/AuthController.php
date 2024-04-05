3bc5c42ffc6783af21bc666a15f847a015e39217
Nozell77 <rafisidqi999@gmail.com>
2024-04-06 00:07:38 +0700

    add register, login and reset password

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\ResetPassword as RequestsResetPassword;
+use App\Models\Otp;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $request->validated();
+
+        $userData = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ];
+
+        $user = User::create($userData);
+        $token = $user->createToken('tedikap')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $request->validated();
+
+        $user = User::whereEmail($request->email)->first();
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+
+        $token = $user->createToken('forumapp')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+    public function resetPassword(RequestsResetPassword $request)
+    {
+        $request->validate([
+            'email' => 'required|email|max:255',
+            'otp' => 'required|numeric',
+            'password' => 'required|min:6',
+        ]);
+
+        $otpData = Otp::where('email', $request->email)->first();
+
+        if (! $otpData) {
+            return response([
+                'message' => 'OTP not found',
+            ], 404);
+        }
+
+        if ($otpData->otp != $request->otp) {
+            return response([
+                'message' => 'Invalid OTP',
+            ], 400);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        $user->password = bcrypt($request->password);
+        $user->save();
+        $otpData->delete();
+
+        return response([
+            'message' => 'Password has been reset successfully',
+        ], 200);
+    }
+}
